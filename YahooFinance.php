9a4683603dd9bc693a06153dd2cc127d16c6bbb6
August Yip <augustyip@gmail.com>
2014-10-27 05:45:54 -0400

    Add method function quote().

--- YahooFinance.php
+++ YahooFinance.php
@@ -5,13 +5,52 @@ class YahooFinance {
 
   // YQL Query Parameters. Ref: https://developer.yahoo.com/yql/guide/yql_url.html
   private $options = array(
-    'format' => 'json',
     'env' => 'http://datatables.org/alltables.env',
   );
 
-  private $format;
-
   public function __construct($options = array()) {
-    
+    $this->options = array_merge($this->options, $options);
+  }
+
+  public function quote($symbols) {
+    if (is_string($symbols)) {
+      $symbols = array($symbols);
+    }
+
+    $this->options['q'] = 'SELECT * FROM yahoo.finance.quote WHERE symbol IN ("' . implode('", "', $symbols) . '")';
+    return $this->query();
+  }
+
+  private function query() {
+    if (empty($this->options['q']))
+      return;
+
+    $url = $this->yql_url . '?' . $this->http_build_query($this->options);
+    $curl = curl_init($url);  
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    return curl_exec($curl);
+  }
+
+  private function http_build_query(array $query, $parent = '') {
+    $params = array();
+
+    foreach ($query as $key => $value) {
+      $key = ($parent ? $parent . '[' . rawurlencode($key) . ']' : rawurlencode($key));
+
+      // Recurse into children.
+      if (is_array($value)) {
+        $params[] = $this->http_build_query($value, $key);
+      }
+      // If a query parameter value is NULL, only append its key.
+      elseif (!isset($value)) {
+        $params[] = $key;
+      }
+      else {
+        // For better readability of paths in query strings, we decode slashes.
+        $params[] = $key . '=' . str_replace('%2F', '/', rawurlencode($value));
+      }
+    }
+
+    return implode('&', $params);
   }
 }
